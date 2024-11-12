c264dec1847c19ed31e49cca4f9148e7d879defa
Sharad <sharadkumars035@gmail.com>
2024-11-12 15:53:02 +0530

    All Files

---
+++ DOM/app.js
@@ -0,0 +1,17 @@
+const h1 = document.getElementById("head");
+console.log(h1);
+
+const paras = document.getElementsByClassName("para");
+console.log(paras);
+
+const paraByTag = document.getElementsByTagName("p");
+console.log(paraByTag);
+
+const h1ById = document.querySelector("#head");
+console.log(h1ById);
+
+const pByQuerry = document.querySelector(".para");
+console.log(pByQuerry);
+
+const allPByQuerry = document.querySelectorAll(".para");
+console.log(allPByQuerry);
